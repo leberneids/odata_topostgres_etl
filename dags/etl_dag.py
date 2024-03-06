eb09aa61e94dde98bdf5bd7e62a9fc4413b00180
Lluís Barnadas <lbarnadas@gnhearing.com>
2024-03-06 15:54:53 +0100

    change of column names

--- dags/etl_dag.py
+++ dags/etl_dag.py
@@ -6,6 +6,8 @@ import requests
 from requests.auth import HTTPBasicAuth
 import pandas as pd
 from sqlalchemy import create_engine
+import logging
+from pydantic import BaseModel
 
 default_args = {
     'owner': 'lluis',
@@ -13,7 +15,6 @@ default_args = {
     'start_date': datetime(2024, 1, 1),
     'catchup': False,
 }
-
 
 # List of dictionaries containing task information
 levels = [
@@ -23,36 +24,73 @@ levels = [
     {"url": "https://analytics.dev.azure.com/GNHearing/_odata/v3.0/WorkItems?%24filter=Project%2FProjectName+eq+%27GNH%27+and+Parent%2FParent%2FParent%2FWorkItemId+ne+null+and+Parent%2FParent%2FParent%2FArea%2FAreaPath+eq+%27GNH%27+and+Parent%2FParent%2FParent%2FWorkItemType+eq+%27Portfolio+Epic%27+and+Parent%2FState+ne+%27Closed%27+and+Parent%2FState+ne+%27Removed%27+and+Parent%2FParent%2FState+ne+%27Closed%27+and+Parent%2FParent%2FState+ne+%27Removed%27+and+Parent%2FParent%2FParent%2FState+ne+%27Closed%27+and+Parent%2FParent%2FParent%2FState+ne+%27Removed%27+and+Parent%2FParent%2FParent%2FParent%2FWorkItemId+eq+null&%24select=WorkItemId%2CWorkItemType%2CTitle%2CState%2CCustom_ReasonForMustDo%2CParentWorkItemId%2CGNH_ValueType%2CCustom_RequestforPI%2CCustom_Prioritized%2CCustom_TestAutomationEffort%2CCustom_TestManuelEffort%2CCustom_TestSystemEffort%2CGNH_AppDev_Effort%2CGNH_AppSystem_Effort%2CCustom_LabellingEffort%2CCustom_MarketAccessEffort%2CCustom_RegulatoryCertificationEffort%2CCustom_RegulatoryProductComplianceEffort%2CGNH_FT_Effort%2CGNH_OS_Effort%2CGNH_Sound_Effort%2CGNH_System_Effort%2CGNH_Dev_Effort%2CGNH_QA_Effort%2CCustom_FeatureElectronicIntegration%2CCustom_FeatureRadioSystems%2CCustom_FeatureMaterialsandReliability%2CCustom_FeatureMechanicalDevelopmentCPH%2CCustom_FeatureMechanicalDevelopmentXMN%2CCustom_FeatureAcousticDevelopmentCPH%2CCustom_FeatureAcousticDevelopmentXMN%2CCustom_FeatureQualificationXMN", "level": 3},
 ]
 
+name_change = {"Custom_FeatureAcousticDevelopmentCPH": "HW Acoustic Development CPH", 
+             "Custom_FeatureAcousticDevelopmentXMN": "HW Acoustic Development XMN", 
+             "Custom_FeatureElectronicIntegration": "HW Electronic Integration", 
+             "Custom_FeatureMaterialsandReliability": "HW Materials and Reliability",
+             "Custom_FeatureMechanicalDevelopmentCPH": "HW Mechanical Development CPH",
+             "Custom_FeatureMechanicalDevelopmentXMN": "HW Mechanical Development XMN",
+             "Custom_FeatureQualificationXMN": "HW Qualification XMN",
+             "Custom_FeatureRadioSystems": "HW Radio Systems",
+             "Custom_LabellingEffort": "CQ Labelling",
+             "Custom_MarketAccessEffort": "CQ Market Access",
+             "Custom_RegulatoryCertificationEffort": "CQ Regulatory Certification",
+             "Custom_RegulatoryProductComplianceEffort": "CQ Regulatory Product Compliance",
+             "Custom_TestAutomationEffort": "APP Test Automation",
+             "Custom_TestManuelEffort": "APP Test Manuel",
+             "Custom_TestSystemEffort": "APP Test System",
+             "GNH_AppDev_Effort": "APP Development",
+             "GNH_AppSystem_Effort": "APP System",
+             "GNH_Dev_Effort": "HCP Development",
+             "GNH_FT_Effort": "DSW FT",
+             "GNH_OS_Effort": "DSW OS",
+             "GNH_QA_Effort": "HCP Test",
+             "GNH_Sound_Effort": "DSW Sound",
+             "GNH_System_Effort": "DSW System",
+             "Custom_LifeCycleManagementEffort": "CQ Life Cycle Management"}
+
 
 @task
 def extract_data(url: str, level: int):
-    azure_devops_conn_id = 'azure_devops'
-    azure_devops_conn = BaseHook.get_connection(azure_devops_conn_id)
-    user = azure_devops_conn.login
-    password = azure_devops_conn.password
+    try:
+        azure_devops_conn_id = 'azure_devops'
+        azure_devops_conn = BaseHook.get_connection(azure_devops_conn_id)
+        user = azure_devops_conn.login
+        password = azure_devops_conn.password
 
-    response = requests.get(url, auth=HTTPBasicAuth(user, password))
-    data = response.json()
-    df = pd.json_normalize(data['value'])
-    tbl_name = f"level_{level}"
+        response = requests.get(url, auth=HTTPBasicAuth(user, password))
+        data = response.json()
+        df = pd.json_normalize(data['value'])
+        df = df.rename(columns=name_change)
+        tbl_name = f"level_{level}"
     
-    # Return DataFrame as JSON and table name for next task
-    return df.to_json(orient='split'), tbl_name
+        logging.info(f"Data extracted successfully for level {level}")
+        return df.to_json(orient='split'), tbl_name
+    except requests.exceptions.HTTPError as http_err:
+        logging.error(f"HTTP error occurred: {http_err}")  # HTTP error
+    except Exception as err:
+        logging.error(f"An error occurred: {err}")  # Other errors
+
 
 @task
 def load_to_db(data):
-    df_json, tbl_name = data  # Now you unpack it inside the task
-    postgres_external_conn_id = 'postgres_external'
-    postgres_conn = BaseHook.get_connection(postgres_external_conn_id)
-    user = postgres_conn.login
-    password = postgres_conn.password
-    database = 'Azuredevops'
-    
-    engine = create_engine(f'postgresql://{user}:{password}@host.docker.internal:5432/{database}')
-    df = pd.read_json(df_json, orient='split')
-    
-    # Write DataFrame to the SQL database
-    df.to_sql(tbl_name, engine, if_exists='replace', index=False)
+    try:
+        df_json, tbl_name = data  # Now you unpack it inside the task
+        postgres_external_conn_id = 'postgres_external'
+        postgres_conn = BaseHook.get_connection(postgres_external_conn_id)
+        user = postgres_conn.login
+        password = postgres_conn.password
+        database = 'Azuredevops'
+        
+        engine = create_engine(f'postgresql://{user}:{password}@host.docker.internal:5432/{database}')
+        df = pd.read_json(df_json, orient='split')
+        
+        # Write DataFrame to the SQL database
+        df.to_sql(tbl_name, engine, if_exists='replace', index=False)
+
+        logging.info(f"Data loaded successfully into {tbl_name}")
+    except Exception as err:
+        logging.error(f"Failed to load data into database: {err}")
 
 with DAG('odata_to_postgres', default_args=default_args, schedule_interval='@daily', catchup=False) as dag:
     for level_info in levels:
